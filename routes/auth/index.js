cf5edc684b9129f7005300000f87acf10f7d0689
aakatev <aakatev@mail.sfsu.edu>
2020-03-31 12:21:00 -0700

    Signin route and form

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const { registerUser } = require('../../db/user');
 const router = express.Router();
+const passport = require('../../config/auth');
 
 router.get("/signin", (req, res) => {
   res.render("signin", { title: "Sign In" });
 });
+
+router.post('/signin',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true })
+);
 
 router.get("/signup", (req, res) => {
   res.render("signup", { title: "Sign Up" });
