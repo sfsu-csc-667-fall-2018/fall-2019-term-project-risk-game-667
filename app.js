2b601bd59e5b16a3b6f58bf240bd7d87eae5a648
aakatev <aakatev@mail.sfsu.edu>
2020-03-31 12:08:35 -0700

    Passport middleware hooked up; Express session added

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
-}
+const session = require('express-session');
+const passport = require('./config/auth');
 
 const authRouter = require("./routes/auth");
 const appRouter = require("./routes/app");
@@ -21,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ 
+  secret: 'e93259e3e7d30df0f2a11fb9f32b41a2a9bb6fa39c5bcfc031200c8e32de68a3', //TODO replace with environmental variable
+  resave: true,
+  saveUninitialized: true
+})); 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", appRouter);
 app.use("/", authRouter);
--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
-const strategy = require('passport-local').Strategy;
+const Strategy = require('passport-local').Strategy;
 const { findUser, validatePassword } = require('../db/user');
 
-module.exports = passport.use(new LocalStrategy(
+module.exports = passport.use(new Strategy(
   async function(username, password, next) {
     let result = findUser('username', username); 
     if (result.error) { 
