108e72ee07fcd61e746d3f52490ab204844c6d27
aakatev <aakatev@mail.sfsu.edu>
2020-03-31 12:45:28 -0700

    Typos fixed

--- config/auth.js
+++ config/auth.js
@@ -2,15 +2,24 @@ const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 const { findUser, validatePassword } = require('../db/user');
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 module.exports = passport.use(new Strategy(
   async function(username, password, next) {
-    let result = findUser('username', username); 
+    let result = await findUser('username', username); 
+    console.log(result); //TODO remove debugging
     if (result.error) { 
       return next(result); 
     }
     if (validatePassword(result.password, password)) {
       return next(null, false, { message: 'Incorrect password.' });
     }
-    return next(null, user);
+    return next(null, result);
   }
 ));
