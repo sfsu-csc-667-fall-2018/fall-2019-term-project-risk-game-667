ca3a7ddb4e302887ba5f6d64c5988f1a30eeaff2
aakatev <aakatev@mail.sfsu.edu>
2020-04-01 15:00:49 -0700

    Hacky broadcast added to chat

--- config/ws.js
+++ config/ws.js
@@ -1,15 +1,19 @@
 const chat = require('../db/chat');
 
 let wsHandler = async function connection(ws) {
+  let wss = this; //TOFIX kinda hacky... or not?
+
   let currentMessages = await chat.getMessages('chat_id', 'lobby', 0, 200);
   ws.send(JSON.stringify(currentMessages));
 
   ws.on('message', async function incoming(data) {
     let newMessage = JSON.parse(data);
     let processMessage = await chat.sendMessage(newMessage.body, newMessage.senderId, newMessage.chatId);
-    
     let currentMessages = await chat.getMessages('chat_id', 'lobby', 0, 200);
-    ws.send(JSON.stringify(currentMessages));    
+      
+    wss.clients.forEach(client => {
+      client.send(JSON.stringify(currentMessages));        
+    });
     // TODO redesign this logic
     // use some protocol to define messages 
     // ws.send(JSON.stringify({
