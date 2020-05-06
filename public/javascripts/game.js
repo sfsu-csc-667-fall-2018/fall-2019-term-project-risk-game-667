ad4a7d80883fc7a0c8b9d67b5bf64e2a7fd08f05
Ryan Shu <ryanshu78@gmail.com>
2020-05-06 01:38:35 -0700

    Added logic to start timer on gameevent git push

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,16 +1,38 @@
 import { h, Component, render } from '../vendor/preact'
 import htm from '../vendor/htm'
+import io from 'socket.io-client'
+import { emitGameEvent } from '../../config/events'
 
 const html = htm.bind(h)
 
+const getRoom = () => {
+  let url = window.location.pathname.split('/') 
+  return  url[url.length-1]
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [],
+      warning: '',
+    }
+    this.socket = io()
+    this.gameId = getRoom()
+  }
+  componentDidMount() {
+    this.socket.on(emitGameEvent(this.gameId), (data) => {
+      console.log('starting timer')
+      this.handleTimerEvent()
+    })
+  }
   handleTimerEvent() {
     var countDownDate = new Date().getTime()+30000;
     var x = setInterval(function() {
       var now = new Date().getTime();
       var distance = countDownDate - now;
       document.getElementById("timer").innerHTML = parseInt((countDownDate - now)/1000) + 's';
+      console.log()
       if (distance < 0) {
         clearInterval(x);
         document.getElementById("timer").innerHTML = "EXPIRED";
@@ -22,7 +44,7 @@ class App extends Component {
     <div style="text-align:left;">
       <div class="container-fliud m-3">
         <h3 class="display-4">Welcome to Risk Game!
-        <p style="float:right;" id="timer" onload="${this.handleTimerEvent()}">
+        <p style="float:right;" id="timer" >
       </p></h3>
         <a href="/lobby">Back to lobby</a>
       </div>
