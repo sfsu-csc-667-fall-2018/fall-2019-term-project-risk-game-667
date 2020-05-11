238a88efd5dfe23fbb476d184ac093aafce2af1c
aakatev <aakatev@mail.sfsu.edu>
2020-05-11 14:07:43 -0700

    Vue app modified

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    game: './public/javascripts/game.js',
     chat: './public/javascripts/chat.js',
     landing: './public/javascripts/landing.js',
     navbar: './public/javascripts/navbar.js',
